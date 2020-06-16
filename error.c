f117dfc0f5ad97276f11b4b5e97d98ded7870233
Peter NTSUNTSHA <pntsunts@c4r21s5.wethinkcode.co.za>
2020-06-16 11:52:10 +0200

    update

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/15 14:29:58 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/06/16 11:29:11 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/06/16 11:51:53 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,12 @@ void	ft_error(t_files **data, char *Path)
 		ft_putstr(Path);
 		ft_putstr(" : Permission Denied");
 		exit(0);
+	}
+	if (errno == 2)
+	{
+		ft_putstr("ft_ls: ");
+		ft_putstr(Path);
+		ft_putstr(" : No such file or Diretory");
+		exit (0);
 	}
 }
